ba0939a8de2e713b0fff4cc301d34527c4c0fc97
Excoinsevm <149539033+Excoinsevm@users.noreply.github.com>
2024-10-13 16:41:21 +0100

    Update contracts.js

--- enum/contracts.js
+++ enum/contracts.js
@@ -2,9 +2,9 @@ const UtilsAddresses = require("../utils/addresses");
 const EnumChainId = require("./chain.id");
 
 const EnumContracts = {
-    [EnumChainId.BSC]: {
-        MAIN_ROUTER: UtilsAddresses.toCheckSum("0x10ED43C718714eb63d5aA57B78B54704E256024E"), // Pancake
-        MAIN_FACTORY: UtilsAddresses.toCheckSum("0xca143ce32fe78f1f7019d7d551a6402fc5350c73") // Pancake
+    [EnumChainId.BROCK]: {
+        MAIN_ROUTER: UtilsAddresses.toCheckSum("0x28011841A9E8D782a3B58da3e757fc939cca84B3"), // Pancake
+        MAIN_FACTORY: UtilsAddresses.toCheckSum("0x195b605fa7c6f379fd27ddeec89cfae6caabfae9") // Pancake
     }
 };
 
